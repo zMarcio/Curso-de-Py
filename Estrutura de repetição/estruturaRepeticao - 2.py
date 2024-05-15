a993e13c3b7f75cc2783b9bd611d7929d5fb2f72
Márcio <marciopaulolima21@edu.unifor.br>
2024-05-15 17:01:25 -0300

    commitando todos os códigos disponiveis.

---
+++ Estrutura de repetição/estruturaRepeticao - 2.py
@@ -0,0 +1,10 @@
+# 1) Escreva um programa que peça dois números inteiros e imprima todos os números inteiros entre eles.
+
+
+num1 = int(input('digite um numero: '))
+num2 = int(input('digite um segundo numero: '))
+
+for i in range(num1,num2):
+    print(i)
+
+
