25daee7d9f822cbef8beb8cf33d9c8b5485bef28
Márcio <marciopaulolima21@edu.unifor.br>
2024-05-15 19:54:38 -0300

    Alteração em algumas partes

--- Estrutura de repetição/estruturaRepeticao - 8.py
+++ Estrutura de repetição/estruturaRepeticao - 8.py
@@ -4,13 +4,22 @@
 
 entrada = int(input('Diga sua idade: '))
 
-if entrada > 0 and entrada < 25:
-    print(f'Idade: {entrada} [0-25]')
-if entrada > 26 and entrada < 50:
-    print(f'Idade: {entrada} [26-50]')
-if entrada > 51 and entrada < 75:
-    print(f'Idade: {entrada} [51-75]')
-if entrada > 76 and entrada < 100:
-    print(f'Idade: {entrada} [76-100]')
-else:
-    print('Fora do escopo!')
+idade_0_25 = 0
+idade_26_50 = 0
+idade_51_75 = 0
+idade_76_100 = 0
+
+while entrada >=:
+    if entrada > 0 and entrada < 25:
+        idade_0_25 += 1 
+    if entrada > 26 and entrada < 50:
+        idade_26_50 += 1
+    if entrada > 51 and entrada < 75:
+        idade_51_75 += 1
+    if entrada > 76 and entrada < 100:
+        idade_76_100 += 1
+
+print(f'pessoa entre [0,25]:  {idade_0_25}')
+print(f'pessoa entre [26,50]:  {idade_26_50}')
+print(f'pessoa entre [51,75]:  {idade_51_75}')
+print(f'pessoa entre [76,100]:  {idade_76_100}')
