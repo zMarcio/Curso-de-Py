cd026657d7bb48b5d5f5619628209727ea423583
Márcio <marciopaulolima21@edu.unifor.br>
2024-05-23 10:31:56 -0300

    Novos códigos

--- listas/desafios/des - 4.py
+++ listas/desafios/des - 4.py
@@ -1,2 +1,9 @@
 # Crie uma lista usando o list comprehension que armazena somente o valor numérico de cada tupla caso o primeiro elemento seja 'Apartamento', 
 # a partir da seguinte lista de tuplas:
+
+
+aluguel = [('Apartamento', 1700), ('Apartamento', 1400), ('Casa', 2150), ('Apartamento', 1900), ('Casa', 1100)]
+p = 0
+comprehension = [i[1] for i in aluguel if type(i[0]) == str]
+
+print(comprehension)
