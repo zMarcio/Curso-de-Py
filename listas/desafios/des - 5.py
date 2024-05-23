12f7368a9f67ca9606f95c5ecbab6ea61b2accb2
Márcio <marciopaulolima21@edu.unifor.br>
2024-05-23 10:37:35 -0300

    Novos códigos

---
+++ listas/desafios/des - 5.py
@@ -0,0 +1,9 @@
+#  Crie um dicionário usando o dict comprehension em que as chaves estão na lista meses = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'] 
+# e os valores estão em despesa = [860, 490, 1010, 780, 900, 630, 590, 770, 620, 560, 840, 360].
+
+meses = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
+despesa = [860, 490, 1010, 780, 900, 630, 590, 770, 620, 560, 840, 360]
+
+comprehension  = {chave:value for chave,value in zip(meses,despesa) }
+
+print(comprehension)
