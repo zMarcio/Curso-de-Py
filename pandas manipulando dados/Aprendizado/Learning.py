cbab64ae6958f5b8f551c39d7a1b5bc17e13b86d
Márcio <marciopaulolima21@edu.unifor.br>
2024-06-03 15:01:40 -0300

    Manipulando dados com pandas

---
+++ pandas manipulando dados/Aprendizado/Learning.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+url = '/content/drive/MyDrive/Ciência de Dados/Data Science Alura/1-SEEG10_GERAL-BR_UF_2022.10.27-FINAL-SITE.xlsx'
+
+data = pd.read_excel(url, sheet_name='GEE Estados',)
+
+data
