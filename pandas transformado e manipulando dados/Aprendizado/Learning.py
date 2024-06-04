16c047959c0d8a55014b3ee419d40ba14ab8d03e
Márcio <marciopaulolima21@edu.unifor.br>
2024-06-04 13:27:43 -0300

    Novos códigos

--- pandas transformado e manipulando dados/Aprendizado/Learning.py
+++ pandas transformado e manipulando dados/Aprendizado/Learning.py
@@ -14,3 +14,29 @@ colunas = list(dados.columns)
 colunas
 dados = dados.explode(colunas[3:])
 dados
+
+dados.reset_index(inplace=True,drop=True)
+dados.head()
+
+dados.info()
+
+dados['max_hospedes'] = dados['max_hospedes'].astype(np.int64)
+dados.info()
+
+col_numericas = ['quantidade_banheiros', 'quantidade_quartos','quantidade_camas']
+dados[col_numericas] = dados[col_numericas].astype(np.int64)
+dados['avaliacao_geral'] = dados['avaliacao_geral'].astype(np.float64)
+dados.info()
+
+dados['preco'] = dados['preco'].apply(lambda x : x.replace('$', '').replace(',','').strip())
+dados['preco'] = dados['preco'].astype(np.float64)
+
+dados['taxa_limpeza'] = dados['taxa_limpeza'].apply(lambda x : x.replace('$', '').replace(',','').strip())
+dados['taxa_limpeza'] = dados['taxa_limpeza'].astype(np.float64)
+
+dados['taxa_deposito'] = dados['taxa_deposito'].apply(lambda x : x.replace('$', '').replace(',','').strip())
+dados['taxa_deposito'] = dados['taxa_deposito'].astype(np.float64)
+
+dados.head()
+
+dados
