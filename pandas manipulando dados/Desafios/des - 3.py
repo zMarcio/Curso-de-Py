8ca114ce0779eaaa1734e8e08f4cdf3ade21095c
Márcio <marciopaulolima21@edu.unifor.br>
2024-06-04 13:20:06 -0300

    Finalizando os desafios

--- pandas manipulando dados/Desafios/des - 3.py
+++ pandas manipulando dados/Desafios/des - 3.py
@@ -8,3 +8,19 @@ colunas_info = list(data.loc[:,'Nível 1 - Setor':'Produto'].columns)
 colunas_emissao = list(data.loc[:,1970:2021].columns)
 
 emissao = data.melt(id_vars = colunas_info, value_vars = colunas_emissao, var_name = 'Ano', value_name = 'Emissão')
+
+setores = emissao.pivot_table(values = 'Emissão', index = 'Ano', columns = 'Nível 1 - Setor', aggfunc = 'mean')
+setores
+
+testing = emissao.groupby(['Estado', 'Nível 1 - Setor'])[['Emissão']].sum()
+testing
+
+testing.xs('Energia', level=1)
+
+testing.xs('MG',level=0).idxmax()
+
+testing = emissao.groupby(['Estado', 'Nível 1 - Setor'])[['Emissão']].sum()
+testing.groupby(level=0).idxmax()
+
+testing = emissao.groupby(['Estado', 'Nível 1 - Setor'])[['Emissão']].sum()
+testing.groupby(level=1).idxmax()
