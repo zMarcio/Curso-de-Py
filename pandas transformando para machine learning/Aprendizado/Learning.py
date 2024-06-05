5e6fb3446f0c890ab7e89e8edf508ed2a942cb1b
Márcio <marciopaulolima21@edu.unifor.br>
2024-06-05 15:24:01 -0300

    Polindo dados para utilizar em machine learning

---
+++ pandas transformando para machine learning/Aprendizado/Learning.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+import json
+
+url = '/content/drive/MyDrive/Ciência de Dados/Data Science Alura/dataset-telecon.json'
+
+# data = pd.read_json(url)
+
+# data = pd.json_normalize(data[['cliente','telefone','internet','conta']])
+
+# cliente = pd.json_normalize(data['cliente'])
+# telefone = pd.json_normalize(data['telefone'])
+# internet = pd.json_normalize(data['internet'])
+# conta = pd.json_normalize(data['conta'])
+
+# data.head()
+
+with open(url) as f:
+  leitura = json.load(f)
+
+leitura
+
+data = pd.json_normalize(leitura)
+
+data.head()
+
+
+data.info()
